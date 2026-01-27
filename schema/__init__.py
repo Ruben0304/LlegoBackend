97900acb6c0d9f41d63135dbabcdb9231e9f19f6
Ruben0304 <hernandzruben9@gmail.com>
2026-01-27 17:47:06 -0500

    refactor: Remove AiAssistantOutputType from schema imports and exports.

--- schema/__init__.py
+++ schema/__init__.py
@@ -7,7 +7,7 @@ from .branches.types import BranchType, CoordinatesType
 from .branches.queries import BranchQuery
 from .products.types import ProductType
 from .products.queries import ProductQuery
-from .ai_assistant.types import AiAssistantResponseType, AiAssistantOutputType, AiAssistantChatInput
+from .ai_assistant.types import AiAssistantResponseType, AiAssistantChatInput
 from .ai_assistant.queries import AiAssistantQuery
 from .orders.types import OrderType, OrderStatusEnum, PaymentStatusEnum
 from .orders.queries import OrderQuery
@@ -28,7 +28,6 @@ __all__ = [
     "ProductType",
     "ProductQuery",
     "AiAssistantResponseType",
-    "AiAssistantOutputType",
     "AiAssistantChatInput",
     "AiAssistantQuery",
     "OrderType",
